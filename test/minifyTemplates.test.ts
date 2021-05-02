3d8dcafb5f2dee989a9892e23158d68aa74947a1
Max Milton <max@wearegenki.com>
2021-05-03 09:32:29 +1000

    Add test

--- test/minifyTemplates.test.ts
+++ test/minifyTemplates.test.ts
@@ -98,6 +98,13 @@ test('reduces all whitespaces to a single space', () => {
   const returned = minifyTemplates(mockBuildResult);
   assert.is(getOutput(returned), 'let a = ` `;');
 });
+test('does not reduce all whitespaces when escaped', () => {
+  const escapedWhitespaces = "' '' '' '\\f\\n\\r\\t\\v\\u00a0\\u1680\\u2000\\u2001\\u2002\\u2003\\u2004\\u2005\\u2006\\u2007\\u2008\\u2009\\u200a\\u2028\\u2029\\u202f\\u205f\\u3000\\ufeff";
+  const source = `let a = \`${escapedWhitespaces}\`;`;
+  const mockBuildResult = createMockBuildResult(source);
+  const returned = minifyTemplates(mockBuildResult);
+  assert.is(getOutput(returned), source);
+});
 
 test('removes single space between tags', () => {
   const mockBuildResult = createMockBuildResult('let a = `> <`;');
