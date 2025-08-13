d6b399be31ad5a0b7457935ca8bc86b8cae291b5
Max Milton <max@happyhackers.xyz>
2025-08-13 23:56:21 +0900

    chore: Run code formatting

--- build.ts
+++ build.ts
@@ -1,68 +1,65 @@
 /* eslint-disable no-console */
 
-import esbuild from 'esbuild';
-import ts from 'typescript';
-import pkg from './package.json' with { type: 'json' };
+import esbuild from "esbuild";
+import ts from "typescript";
+import pkg from "./package.json" with { type: "json" };
 
 const external = Object.keys(pkg.dependencies);
 
-console.time('prebuild');
+console.time("prebuild");
 await Bun.$`rm -rf dist`;
-console.timeEnd('prebuild');
+console.timeEnd("prebuild");
 
 // Node CJS bundle
-console.time('build:1');
+console.time("build:1");
 const out1 = await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  outfile: 'dist/index.js',
-  platform: 'node',
-  target: ['node12'],
+  entryPoints: ["src/index.ts"],
+  outfile: "dist/index.js",
+  platform: "node",
+  target: ["node12"],
   external,
   bundle: true,
   sourcemap: true,
   minify: true,
   metafile: process.stdout.isTTY,
-  logLevel: 'debug',
+  logLevel: "debug",
 });
-console.timeEnd('build:1');
+console.timeEnd("build:1");
 if (out1.metafile) console.log(await esbuild.analyzeMetafile(out1.metafile));
 
 // Node ESM bundle
-console.time('build:2');
+console.time("build:2");
 const out2 = await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  outfile: 'dist/index.mjs',
-  platform: 'node',
-  format: 'esm',
-  target: ['node16'],
+  entryPoints: ["src/index.ts"],
+  outfile: "dist/index.mjs",
+  platform: "node",
+  format: "esm",
+  target: ["node16"],
   external,
   bundle: true,
   sourcemap: true,
   minify: true,
   metafile: process.stdout.isTTY,
-  logLevel: 'debug',
+  logLevel: "debug",
 });
-console.timeEnd('build:2');
+console.timeEnd("build:2");
 if (out2.metafile) console.log(await esbuild.analyzeMetafile(out2.metafile));
 
-console.time('dts');
+console.time("dts");
 const config: ts.CompilerOptions = {
   emitDeclarationOnly: true,
   declaration: true,
   declarationMap: true,
-  declarationDir: 'dist',
+  declarationDir: "dist",
   skipLibCheck: true,
 };
 const result = ts
-  .createProgram(['src/index.ts'], config)
+  .createProgram(["src/index.ts"], config)
   .emit(undefined, undefined, undefined, true);
 if (result.emitSkipped) {
   console.error(
-    ts.formatDiagnosticsWithColorAndContext(
-      result.diagnostics,
-      ts.createCompilerHost(config),
-    ),
+    ts.formatDiagnosticsWithColorAndContext(result.diagnostics, ts.createCompilerHost(config)),
   );
   process.exitCode = 1;
 }
-console.timeEnd('dts');
+console.timeEnd("dts");
--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions, no-plusplus */
 
-import { afterAll, beforeAll, expect, test } from 'bun:test';
-import fs from 'node:fs';
-import path from 'node:path';
-import * as esbuild from 'esbuild';
-import { minifyTemplates, writeFiles } from '../dist/index.js';
-import { createTempDir, deleteTempDir, getTempDir } from './utils.ts';
+import { afterAll, beforeAll, expect, test } from "bun:test";
+import fs from "node:fs";
+import path from "node:path";
+import * as esbuild from "esbuild";
+import { minifyTemplates, writeFiles } from "../dist/index.js";
+import { createTempDir, deleteTempDir, getTempDir } from "./utils.ts";
 
 const context = {};
 beforeAll(() => createTempDir(context));
@@ -15,71 +15,71 @@ let count = 0;
 
 // TODO: Test incremental builds and watch mode
 
-test('build runs without error', async () => {
+test("build runs without error", async () => {
   const dir = getTempDir(context, `test${count++}`);
-  const srcfile = path.join(dir, 'index.ts');
-  const outfile = path.join(dir, 'index.js');
+  const srcfile = path.join(dir, "index.ts");
+  const outfile = path.join(dir, "index.js");
   await fs.promises.writeFile(
     srcfile,
     `
     const a = \` \n\n\n   <a>b   </a>   \n\n  \`;
     console.log(a);`,
-    'utf8',
+    "utf8",
   );
   await esbuild.build({
     entryPoints: [srcfile],
     outfile,
     bundle: true,
-    platform: 'node',
+    platform: "node",
     minifyWhitespace: true,
     write: false,
     plugins: [minifyTemplates(), writeFiles()],
   });
-  const result = await fs.promises.readFile(outfile, 'utf8');
+  const result = await fs.promises.readFile(outfile, "utf8");
   // TODO: Use toMatchInlineSnapshot once bun test supports it
-  expect(result).toBe('var a=`<a>b </a>`;console.log(a);\n');
+  expect(result).toBe("var a=`<a>b </a>`;console.log(a);\n");
 });
 
-test('does not minify when build write is true', async () => {
+test("does not minify when build write is true", async () => {
   const dir = getTempDir(context, `test${count++}`);
-  const srcfile = path.join(dir, 'index.ts');
-  const outfile = path.join(dir, 'index.js');
+  const srcfile = path.join(dir, "index.ts");
+  const outfile = path.join(dir, "index.js");
   const source = `
     const a = \` \n\n\n   <a>b   </a>   \n\n  \`;
     console.log(a);
   `;
-  await fs.promises.writeFile(srcfile, source, 'utf8');
+  await fs.promises.writeFile(srcfile, source, "utf8");
   await esbuild.build({
     entryPoints: [srcfile],
     outfile,
     bundle: true,
-    platform: 'node',
+    platform: "node",
     minifyWhitespace: true,
     write: true,
     plugins: [minifyTemplates(), writeFiles()],
   });
-  const result = await fs.promises.readFile(outfile, 'utf8');
+  const result = await fs.promises.readFile(outfile, "utf8");
   // TODO: Use toMatchInlineSnapshot once bun test supports it
-  expect(result).toBe('var a=` \n\n\n   <a>b   </a>   \n\n  `;console.log(a);\n');
+  expect(result).toBe("var a=` \n\n\n   <a>b   </a>   \n\n  `;console.log(a);\n");
 });
-test('does not minify when build write is the default (undefined)', async () => {
+test("does not minify when build write is the default (undefined)", async () => {
   const dir = getTempDir(context, `test${count++}`);
-  const srcfile = path.join(dir, 'index.ts');
-  const outfile = path.join(dir, 'index.js');
+  const srcfile = path.join(dir, "index.ts");
+  const outfile = path.join(dir, "index.js");
   const source = `
     const a = \` \n\n\n   <a>b   </a>   \n\n  \`;
     console.log(a);
   `;
-  await fs.promises.writeFile(srcfile, source, 'utf8');
+  await fs.promises.writeFile(srcfile, source, "utf8");
   await esbuild.build({
     entryPoints: [srcfile],
     outfile,
     bundle: true,
-    platform: 'node',
+    platform: "node",
     minifyWhitespace: true,
     plugins: [minifyTemplates(), writeFiles()],
   });
-  const result = await fs.promises.readFile(outfile, 'utf8');
+  const result = await fs.promises.readFile(outfile, "utf8");
   // TODO: Use toMatchInlineSnapshot once bun test supports it
-  expect(result).toBe('var a=` \n\n\n   <a>b   </a>   \n\n  `;console.log(a);\n');
+  expect(result).toBe("var a=` \n\n\n   <a>b   </a>   \n\n  `;console.log(a);\n");
 });
