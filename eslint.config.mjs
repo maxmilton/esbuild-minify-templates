1e5d7f7f68093dfb2b0a94e4496fd6e3b360f361
Max Milton <max@happyhackers.xyz>
2025-08-13 23:50:04 +0900

    chore: Refactor linting and tooling configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import eslint from '@eslint/js';
-import mm from '@maxmilton/eslint-config';
-import unicorn from 'eslint-plugin-unicorn';
-import ts from 'typescript-eslint';
+import js from "@eslint/js";
+import mm from "@maxmilton/eslint-config";
+import unicorn from "eslint-plugin-unicorn";
+import ts from "typescript-eslint";
 
 const OFF = 0;
 const WARN = 1;
 const ERROR = 2;
 
 export default ts.config(
-  eslint.configs.recommended,
-  ...ts.configs.strictTypeChecked,
-  ...ts.configs.stylisticTypeChecked,
+  js.configs.recommended,
+  ts.configs.strictTypeChecked,
+  ts.configs.stylisticTypeChecked,
   unicorn.configs.recommended,
   mm.configs.recommended,
   {
@@ -19,21 +19,20 @@ export default ts.config(
     },
     languageOptions: {
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ['*.mjs'],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
-      'unicorn/import-style': OFF,
-      'unicorn/prefer-global-this': OFF, // prefer to clearly separate Bun and DOM
-      'unicorn/prefer-node-protocol': OFF, // incompatible with older node versions
-      'unicorn/prefer-string-replace-all': OFF, // incompatible with older node versions
-      'unicorn/prefer-top-level-await': WARN, // incompatible with older node versions
+      quotes: [ERROR, "double", { avoidEscape: true }],
+      "unicorn/import-style": OFF,
+      "unicorn/prefer-global-this": OFF, // prefer to clearly separate Bun and DOM
+      "unicorn/prefer-node-protocol": OFF, // incompatible with older node versions
+      "unicorn/prefer-string-replace-all": OFF, // incompatible with older node versions
+      "unicorn/prefer-top-level-await": WARN, // incompatible with older node versions
     },
   },
   {
-    ignores: ['**/*.bak', 'coverage', 'dist'],
+    ignores: ["**/*.bak", "coverage", "dist"],
   },
 );
